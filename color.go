cf15754b0dc15707c029da7cf11cf4d1a737297d
shelepuginivan <shelepuginivanm@gmail.com>
2024-11-22 18:15:09 +0300

    fix(color): handle short hexadecimal colors

--- color.go
+++ color.go
@@ -18,17 +18,29 @@ func New(r, g, b uint8) *Color {
 }
 
 // NewFromHex returns a new instance of [Color] by parsing hexadecimal color
-// string. The string may start with hash character (`#`). E.g. `f1f2f3` and
-// `#f1f2f3` are both valid arguments.
+// string. The string may start with hash character (`#`) and may be either
+// short or long hexadecimal color. E.g. `fff`, `#fff`, `ffffff`, `#ffffff` are
+// all valid arguments.
 func NewFromHex(hex string) (*Color, error) {
 	i := 0
 	if hex[0] == '#' {
 		i = 1
 	}
 
-	hex_r := hex[i : i+2]
-	hex_g := hex[i+2 : i+4]
-	hex_b := hex[i+4 : i+6]
+	var hex_r, hex_g, hex_b string
+
+	switch len(hex) - i {
+	case 3: // Short hexadecimal notation, e.g. `#abc`.
+		hex_r = string([]byte{hex[i], hex[i]})
+		hex_g = string([]byte{hex[i+1], hex[i+1]})
+		hex_b = string([]byte{hex[i+2], hex[i+2]})
+	case 6: // Long hexadecimal notation, e.g. `#aabbcc`
+		hex_r = hex[i : i+2]
+		hex_g = hex[i+2 : i+4]
+		hex_b = hex[i+4 : i+6]
+	default:
+		return nil, fmt.Errorf("invalid hexadecimal string")
+	}
 
 	r, err := strconv.ParseUint(hex_r, 16, 64)
 	if err != nil {
