820c217061979d37858ccadd11cb5b70beeb10c9
shelepuginivan <shelepuginivanm@gmail.com>
2025-09-21 21:43:46 +0300

    docs(gradient): add godoc comments

--- gradient/gradient.go
+++ gradient/gradient.go
@@ -1,3 +1,4 @@
+// Package gradient provides methods for creating color gradients.
 package gradient
 
 import (
@@ -6,7 +7,11 @@ import (
 	"github.com/shelepuginivan/color"
 )
 
+// Gradient is a common interface for different gradient types.
 type Gradient interface {
+	// Colors calculates the given number of intermediate colors.
 	Colors(steps int) []color.Color
+
+	// Render renders gradient onto image.
 	Render(img image.Image)
 }
